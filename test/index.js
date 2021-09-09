700c06779954154b1a24267b95ed70aa06c3d6aa
Charlie_mac <tchoi.tab@gmail.com>
2021-09-10 00:28:22 +0900

    coplit test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,26 @@
-let obj1 = { foo: 'bar', x: 42 };
-let obj2 = { foo: 'baz', y: 13 };
+function compressString(str) {
+  // TODO: 여기에 코드를 작성합니다.
+  let result = [str[0], ];
+  let prv = { char: str[0], count: 1 };
+  for( let i = 1 ; i < str.length ; i++ )
+  {
+    result.push( str[i] );
+    if( str[i] !== prv.char )
+    {
+      if( prv.count >= 3 )
+      {
+        for( let del = 0 ; del <= prv.count ; del++ ) result.pop();
+        result.push( prv.count, prv.char, str[i] );
+      }
+      console.log('called');
+      prv.char = str[i];
+      prv.count = 1;
+      continue;
+    }
+    prv.count++;
+  }
 
-let clonedObj = { ...obj1 };
-let mergedObj = { ...obj1, ...obj2 };
+  return result.join('');
+}
+
+console.log(compressString('wwwggopp'));
