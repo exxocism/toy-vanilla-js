47f3b3bb5da0cf6809b58159d2814faede96e985
Charlie_eGPU <tchoi.tab@gmail.com>
2021-09-12 15:31:10 +0900

    algorithm exercise

--- vanilla-sample/text.js
+++ vanilla-sample/text.js
@@ -1,2 +1,20 @@
-//let doge = () => { alert('화성 갈끄니까 😁'); }
-//doge();
+function telephoneCheck(str) {
+  
+  let tel = [
+    /^(1[\s-])*[0-9]{3}([-\s])[0-9]{3}\2[0-9]{4}$/,
+    /^(1[\s-]*)*\([0-9]{3}\)[\s]{0,1}[0-9]{3}[-\s][0-9]{4}$/,
+    /^[0-9]{10}$/
+  ];
+  
+  return tel.some( (e, i) => {
+    
+    if ( e.test( str ) ) 
+    {
+      console.log( i + "th index called");
+    }
+    return e.test( str );
+  } );
+}
+
+//console.log(telephoneCheck("555-555-5555"));
+console.log(telephoneCheck("(555-555-5555"));
