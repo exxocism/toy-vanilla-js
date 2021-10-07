2f89b741856e526744be92256a4b2746efe879cc
Charlie_eGPU <tchoi.tab@gmail.com>
2021-10-07 16:01:30 +0900

    code

--- vanilla-sample/test.js
+++ vanilla-sample/test.js
@@ -120,3 +120,28 @@ function countWithVar () {
   }
 }
 countWithVar();
+
+
+let dfs = function (node) {
+  // TODO: 여기에 코드를 작성합니다.
+  let result = [node.value];
+  if( node.hasOwnProperty('children') && node.children.length > 0 ) {
+    for( let i = 0 ; i < node.children.length ; i ++ ) {
+      result.push( ...dfs( node.children[i] ) );
+    }
+  }
+  return result;
+};
+
+// 이 아래 코드는 변경하지 않아도 됩니다. 자유롭게 참고하세요.
+let Node = function (value) {
+  this.value = value;
+  this.children = [];
+};
+
+// 위 Node 객체로 구성되는 트리는 매우 단순한 형태의 트리입니다.
+// membership check(중복 확인)를 따로 하지 않습니다.
+Node.prototype.addChild = function (child) {
+  this.children.push(child);
+  return child;
+};
