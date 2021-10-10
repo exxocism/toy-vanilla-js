3ed279ce39dd00b443ee5dc7825f6a56de50ccb8
eGPUbook_windows <tchoi.tab@gmail.com>
2021-10-10 21:08:29 +0900

    powerSet is well known

--- vanilla-sample/function.js
+++ vanilla-sample/function.js
@@ -1,89 +1,51 @@
-function barcode(len) {
-  // TODO: 여기에 코드를 작성하세요.
-  const barcode_info = Array(len).fill(null);
-
-  for( let i = 0; i < len; i++ ) {
-    const shit = {
-      num: undefined,
-      tried: []
-    };
-    barcode_info[i] = shit;
-  }
-
-  const BARCODE_SHOULD_FALL_BACK = -1;
-
-  const getNextBarcode = (idx) => {
-    for (let i = 1; i <= 3; i++) {
-      if (
-        !barcode_info[idx].hasOwnProperty('tried') ||
-        !barcode_info[idx].tried.includes(i)
-      )
-        return i;
+const powerSet = function (str) {
+  // TODO: 여기에 코드를 작성합니다.
+  const getCombinations = (array, selectNumber) => {
+    const results = [];
+    if (selectNumber === 1) {
+      return array.map((element) => [element]);
     }
-    return BARCODE_SHOULD_FALL_BACK;
-  };
-
-  const getLastGoodBarcode = (idx) => {
-    for (let i = idx; i >= 0; i--) {
-      if (barcode_info[i].tried.length < 3) return i;
-    }
-  };
-
-  const resetBarcodesAndTriedRecords = (idx) => {
-    for (let i = idx; i < barcode_info.length; i++) {
-      // if (barcode_info[i].hasOwnProperty("num")) delete barcode_info[i].num;
-      if (barcode_info[i].hasOwnProperty("num")) barcode_info[i].num = undefined;
-      if (barcode_info[i].hasOwnProperty("tried")) barcode_info[i].tried = [];
-    }
-  };
-
-  const getBarcodeString = () => {
-    let result = "";
-    barcode_info.some((e) => {
-      if (!e.hasOwnProperty("num") || !e.num ) return true;
-      result += e.num;
+    array.forEach((fixed, index, origin) => {
+      const rest = origin.slice(index + 1);
+      const combinations = getCombinations(rest, selectNumber - 1);
+      const attached = combinations.map((combination) => [
+        fixed,
+        ...combination,
+      ]);
+      results.push(...attached);
     });
-    return result;
+    return results;
   };
 
-  const STRING_IS_VALID = -1;
-  const hasStringDuplicatedNumber = (str) => {
-    //const magicExp = /([0-9]+)\1/g;
-    const magicExp = /([0-9]+)(?=\1)/g;
-    const matchStr = str.match(magicExp);
-    if (matchStr !== null ) {
-      return str.lastIndexOf(matchStr);
-    }
-    return STRING_IS_VALID;
-  };
+  const arr_str = Array.from(str);
+  const remove_duplicates = new Set( arr_str );
+  const arr_filtered = [...remove_duplicates];
 
-  let i = 0;
-  while (i >= 0 && i < len) {
-    const barcodeSearched = getNextBarcode(i);
-
-    if (barcodeSearched === BARCODE_SHOULD_FALL_BACK) {
-      i = getLastGoodBarcode(i);
-      resetBarcodesAndTriedRecords(i + 1);
-      continue;
-    }
-
-    barcode_info[i].num = barcodeSearched;
-    barcode_info[i].tried.push(barcodeSearched);
-
-    const duplicatedSearch = hasStringDuplicatedNumber(getBarcodeString());
-    if (duplicatedSearch !== STRING_IS_VALID) {
-      i = getLastGoodBarcode(i);
-      resetBarcodesAndTriedRecords(i + 1);
-      continue;
-    }
-    i++;
+  let storage_combinations = [];
+  for( let i = 1; i <= arr_filtered.length; i++ ) {
+    storage_combinations.push( ...getCombinations( arr_filtered, i ).map( e => e.join('') ) );
   }
-  return getBarcodeString();
-}
+  
+  // storage_combinations.sort( (a, b) => {
+  //   const minimum_length = Math.min( a.length, b.length );
+  //   for( let i = 0; i < minimum_length; i++ ){
+  //     if( a[i] > b[i] ) return 1;
+  //     if( a[i] < b[i] ) return -1;
+  //   }
+  //   if( a.length === b.length ) return 0;
+  //   return (a.length >  b.length)? 1:-1;
+  // });
+  storage_combinations = storage_combinations.map( e => {
+    const word_to_sort = Array.from( e );
+    word_to_sort.sort();
+    return word_to_sort.join('');
+  });
+  storage_combinations.sort();
+  storage_combinations.unshift('');
+  return storage_combinations;
+  // console.dir( storage_combinations );
 
-// let output = barcode(3);
-// console.log(output); // "121"
-// output = barcode(7);
-// console.log(output); // "1213121"
-output = barcode(20);
-console.log(output); // "12131231321231213123"
+};
+
+let output1 = powerSet('abc');
+console.log(output1); // ['', 'a', 'ab', 'abc', 'ac', 'b', 'bc', 'c']
--- vanilla-sample/test.js
+++ vanilla-sample/test.js
@@ -1,3 +1,53 @@
+const powerSet = function (str) {
+  // TODO: 여기에 코드를 작성합니다.
+  const getCombinations = (array, selectNumber) => {
+    const results = [];
+    if (selectNumber === 1) {
+      return array.map((element) => [element]);
+    }
+    array.forEach((fixed, index, origin) => {
+      const rest = origin.slice(index + 1);
+      const combinations = getCombinations(rest, selectNumber - 1);
+      const attached = combinations.map((combination) => [
+        fixed,
+        ...combination,
+      ]);
+      results.push(...attached);
+    });
+    return results;
+  };
+
+  const arr_str = Array.from(str);
+  const remove_duplicates = new Set( arr_str );
+  const arr_filtered = [...remove_duplicates];
+
+  let storage_combinations = [];
+  for( let i = 1; i <= arr_filtered.length; i++ ) {
+    storage_combinations.push( ...getCombinations( arr_filtered, i ).map( e => e.join('') ) );
+  }
+  
+  // storage_combinations.sort( (a, b) => {
+  //   const minimum_length = Math.min( a.length, b.length );
+  //   for( let i = 0; i < minimum_length; i++ ){
+  //     if( a[i] > b[i] ) return 1;
+  //     if( a[i] < b[i] ) return -1;
+  //   }
+  //   if( a.length === b.length ) return 0;
+  //   return (a.length >  b.length)? 1:-1;
+  // });
+  storage_combinations = storage_combinations.map( e => {
+    const word_to_sort = Array.from( e );
+    word_to_sort.sort();
+    return word_to_sort.join('');
+  });
+  storage_combinations.sort();
+  storage_combinations.unshift('');
+  return storage_combinations;
+  // console.dir( storage_combinations );
+
+};
+
+
 function barcode(len) {
   // TODO: 여기에 코드를 작성하세요.
   const barcode_info = Array(len).fill(null);
