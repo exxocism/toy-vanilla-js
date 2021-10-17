2d4bf776bfdabd460759884c72cd38bd132af756
Charlie_eGPU <tchoi.tab@gmail.com>
2021-10-18 04:04:17 +0900

    prime password complete

--- vanilla-sample/test.js
+++ vanilla-sample/test.js
@@ -1,3 +1,98 @@
+const primePassword = (curPwd, newPwd) => {
+  // TODO: 여기에 코드를 작성합니다.
+  // curPwd 
+  // newPwd
+  // 1000부터 9999까지의 소수 library를 만든다 
+  // linked list를 만든다
+  // from부터 to까지 갈수 있는지 확인 갯수 
+  
+  const getPrimeNumbers = () => {
+    const primeStorage = Array(10000).fill(true);
+    const onlyPrimes = [];
+    for( let i = 2 ; i < primeStorage.length ; i += 2 ) { primeStorage[i] = false; }
+    for( let i = 3 ; i < primeStorage.length ; i += 2 ) {
+      if( !primeStorage[i] ) continue;
+      if( i > 1000 ) onlyPrimes.push( i );
+      let multiplier = 1;
+      while( multiplier * i < primeStorage.length ) primeStorage[i * multiplier++] = false;
+    }
+    return onlyPrimes;
+  };
+
+  const isMatrixValid = ( prime1, prime2 ) => {
+    const str1 = String( prime1 );
+    const str2 = String( prime2 );
+    let difference = 0;
+    for( let i = 0 ; i < str1.length ; i ++ ) {
+      if( str1[i] !== str2[i] ) difference++;
+    }
+    return (difference === 1)? 1:0;
+  };
+
+  const createPrimeMatrix = ( onlyPrimes ) => {
+    const matrixPrimes = Array(1061).fill(0).map(() => new Array(1061).fill(0));
+    for( let i = 0 ; i < matrixPrimes.length - 1 ; i++ ) {
+      for( let j = i + 1 ; j < matrixPrimes.length ; j ++ ) {
+        if( !isMatrixValid( onlyPrimes[i], onlyPrimes[j] ) ) continue;
+        matrixPrimes[i][j] = 1;
+        matrixPrimes[j][i] = 1;
+      }
+    }
+    return matrixPrimes;
+  };
+
+  const VISITED = 1;
+  const VALUES = 0;
+
+  const createAdjacentList = ( matrix, values ) => {
+    const adjacent_list = [];
+    matrix.forEach((e) => {
+      const obj = [e.map((check, i) => (check? i : null)).filter((check) => check !== null),];
+      adjacent_list.push(obj);
+    });
+    return adjacent_list;
+  };
+
+  const binSearch = ( arr, target, left, right ) => {
+    if (right >= left) {
+        let mid = left + Math.floor((right - left) / 2);
+        if (arr[mid] === target) return mid;
+        if (arr[mid] > target) return binSearch(arr, target, left, mid - 1);
+        return binSearch(arr, target, mid + 1, right);
+    }
+    return -1;
+  };
+
+  let searchQueue = new Set();
+  const bfs_childonly = ( node, depth ) => {
+    if( !node[VALUES].length ) return ;
+    if( depth === 0 ) {
+      node[VALUES].forEach( e => searchQueue.add( e ) );
+      return ;
+    } 
+    return node[VALUES].forEach( child => bfs_childonly( adjacentList[child], depth - 1 ) );
+  };
+
+
+  if( curPwd === newPwd ) return 0;
+  
+  const primeNumbers = getPrimeNumbers();
+  const primeMatrix = createPrimeMatrix( primeNumbers );
+  const adjacentList = createAdjacentList( primeMatrix, primeNumbers );
+
+  const target = binSearch( primeNumbers, newPwd, 0, primeNumbers.length - 1 );
+  let current = binSearch( primeNumbers, curPwd, 0, primeNumbers.length - 1 );
+  let count = 0;
+  bfs_childonly( adjacentList[current], count );
+  while( searchQueue !== null && searchQueue.size ) {
+    if( searchQueue.has(target) ) return count + 1;
+    count++;
+    searchQueue = new Set();
+    bfs_childonly( adjacentList[current], count );
+  }
+  return -1;
+};
+
 const LPS = function (str) {
   // TODO: 여기에 코드를 작성합니다.
   let left = 0;
