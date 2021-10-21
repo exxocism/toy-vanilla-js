c1f069c0e6d39240dc4e67a86ab0bffc9e4f3404
Charlie_eGPU <tchoi.tab@gmail.com>
2021-10-21 23:42:30 +0900

    spiralTraversal

--- vanilla-sample/function.js
+++ vanilla-sample/function.js
@@ -1,133 +1,84 @@
-const inequalityNumber = function (signs) {
+const spiralTraversal = function (matrix) {
   // TODO: 여기에 코드를 작성합니다.
-  const number_info = [];
-  let number_used = new Set();
-  let biggestNumber = 0;
-  let smallestNumber = 0;
-  //assign Info
-  const sign_processed = signs.replace(/\s/g,''); 
-  for( let i = 0 ; i < sign_processed.length + 1 ; i ++ ) {
-    const number_obj = {
-      number: undefined,
-      tried: [],
-      available: []
-    };
-    number_info[i] = number_obj;
+  // matrix 크기의 visited matrix를 만든다.
+  const createMatrix = ( row, col ) => {
+    const newMatrix = [];
+    for( let i = 0 ; i < row ; i++ ) {
+      const column = new Array(col).fill(false);
+      newMatrix.push( column );
+    }
+    return newMatrix;
+  };
+
+  const DIRECTION_OFFSET = 4;
+  const DIRECTION_RIGHT = 0;
+  const DIRECTION_DOWN = 1;
+  const DIRECTION_LEFT = 2;
+  const DIRECTION_UP = 3;
+  const row = matrix.length;
+  const col = matrix[0].length;
+
+  let result = '';
+  let direction = DIRECTION_RIGHT;
+  let visitedCount = 0;  
+  let row_idx = 0;
+  let col_idx = 0;
+  const matrixCount = row * col;
+  const isVisited = createMatrix( row, col );
+  while( visitedCount < matrixCount ) {
+    switch( direction )
+    {
+      case DIRECTION_RIGHT:
+        for( ; col_idx < col ; col_idx++ ) {
+          if( isVisited[row_idx][col_idx] ) break;
+          result += matrix[row_idx][col_idx];
+          isVisited[row_idx][col_idx] = true;
+          visitedCount++;
+        }
+        col_idx--;
+        row_idx++;
+        break;
+      case DIRECTION_DOWN:
+        for( ; row_idx < row ; row_idx++ ) {
+          if( isVisited[row_idx][col_idx] ) break;
+          result += matrix[row_idx][col_idx];
+          isVisited[row_idx][col_idx] = true;
+          visitedCount++;
+        }
+        row_idx--;
+        col_idx--;
+        break;
+      case DIRECTION_LEFT:
+        for( ; col_idx >= 0 ; col_idx-- ) {
+          if( isVisited[row_idx][col_idx] ) break;
+          result += matrix[row_idx][col_idx];
+          isVisited[row_idx][col_idx] = true;
+          visitedCount++;
+        }
+        col_idx++;
+        row_idx--;
+        break;
+      case DIRECTION_UP:
+        for( ; row_idx >= 0 ; row_idx-- ) {
+          if( isVisited[row_idx][col_idx] ) break;
+          result += matrix[row_idx][col_idx];
+          isVisited[row_idx][col_idx] = true;
+          visitedCount++;
+        }
+        row_idx++;
+        col_idx++;
+        break;
+    }
+    direction = ( direction + 1 ) % DIRECTION_OFFSET;
   }
-
-  const getFinishedNumber = ( info ) => {
-    return Number(info.reduce( (str, e) => { str += e.number; return str; }, ''));
-  };
-
-  const resetTriedRecords = ( idx ) => {
-    for( let i = idx ; i < number_info.length ; i++ ){
-      number_info[i].tried = [];
-    }
-  };
-
-  const enumerateNextAvailablity = ( idx ) => {
-    number_info[i].available = [];
-    if( idx === 0 ) {
-      if( sign_processed[0] === '>' ) {
-        number_info[i].available = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-      } else {
-        number_info[i].available = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-      } 
-      return;
-    }
-    const direction = sign_processed[ idx - 1 ];
-    for( let i = 0 ; i < 10 ; i++ ) {
-      if( number_used.has(i) ) continue;
-      check = number_info[idx - 1].number;
-      if( direction === '>' ) {
-        if( check < i ) continue;
-      } else {
-        if( check > i ) continue;
-      }
-      number_info[idx].available.push(i);
-    }
-  };
-
-  const fallBackToLastAvailableIndex = ( idx ) => {
-    for( let i = idx - 1; i >= 0 ; i -- ) {      
-      number_used.delete( number_info[i].number );
-      if( !number_info[i].available.length ) continue;
-      if( number_info[i].tried.length === number_info[i].available.length ) continue;
-      return i;
-    }
-  };
-
-  const FALLBACK_NEEDED = -1;
-  const getNextAvailableNumber = ( idx, ascending ) => {
-    const num_available = number_info[idx].available.length;
-    if( !num_available ) return FALLBACK_NEEDED;
-    if( num_available === number_info[idx].tried.length ) return FALLBACK_NEEDED;
-    if( ascending ) {
-      for( let i = 0 ; i < num_available ; i++ ) {
-        const check = number_info[idx].available[i];
-        if( number_info[idx].tried.includes( check ) ) continue;
-        return check;
-      }
-    } else {
-      for( let i = num_available - 1 ; i >= 0 ; i-- ) {
-        const check = number_info[idx].available[i];
-        if( number_info[idx].tried.includes( check ) ) continue;
-        return check;
-      }
-    }
-  };
-
-  const getNextAvailableNumberByAscending = ( idx ) => {
-    return getNextAvailableNumber( idx, true );
-  };
-  const getNextAvailableNumberByDescending = ( idx ) => {    
-    return getNextAvailableNumber( idx, false );
-  };
-
-  //get by ascending
-  let i = 0;
-  while (i >= 0 && i < number_info.length ) {
-    enumerateNextAvailablity(i);
-    const numberSearched = getNextAvailableNumberByAscending(i);
-
-    if (numberSearched === FALLBACK_NEEDED) {
-      i = fallBackToLastAvailableIndex(i);
-      resetTriedRecords(i + 1);
-      continue;
-    }
-
-    number_info[i].number = numberSearched;
-    number_info[i].tried.push(numberSearched);
-    number_used.add(numberSearched);
-    i++;
-  }
-  smallestNumber = getFinishedNumber( number_info );
-
-  i = 0;
-  resetTriedRecords(i);
-  number_used = new Set();
-  while (i >= 0 && i < number_info.length ) {
-    enumerateNextAvailablity(i);
-    const numberSearched = getNextAvailableNumberByDescending(i);
-
-    if (numberSearched === FALLBACK_NEEDED) {
-      i = fallBackToLastAvailableIndex(i);
-      resetTriedRecords(i + 1);
-      continue;
-    }
-
-    number_info[i].number = numberSearched;
-    number_info[i].tried.push(numberSearched);
-    number_used.add(numberSearched);
-    i++;
-  }
-
-  biggestNumber = getFinishedNumber( number_info );
-
-  //console.log(`smallestNumber: ${smallestNumber}, biggestNumber: ${biggestNumber}`);
-  return biggestNumber - smallestNumber;
+  return result;
 };
 
-output = inequalityNumber('< > > <');
-console.log(output); // --> 876 (897 - 021))
-
+matrix = [
+  ['T', 'y', 'r', 'i'],
+  ['i', 's', 't', 'o'],
+  ['n', 'r', 'e', 'n'],
+  ['n', 'a', 'L', ' '],
+];
+output = spiralTraversal(matrix);
+console.log(output); // --> 'Tyrion Lannister'
--- vanilla-sample/test.js
+++ vanilla-sample/test.js
@@ -1,3 +1,79 @@
+const spiralTraversal = function (matrix) {
+  // TODO: 여기에 코드를 작성합니다.
+  // matrix 크기의 visited matrix를 만든다.
+  const createMatrix = ( row, col ) => {
+    const newMatrix = [];
+    for( let i = 0 ; i < row ; i++ ) {
+      const column = new Array(col).fill(false);
+      newMatrix.push( column );
+    }
+    return newMatrix;
+  };
+
+  const DIRECTION_OFFSET = 4;
+  const DIRECTION_RIGHT = 0;
+  const DIRECTION_DOWN = 1;
+  const DIRECTION_LEFT = 2;
+  const DIRECTION_UP = 3;
+  const row = matrix.length;
+  const col = matrix[0].length;
+
+  let result = '';
+  let direction = DIRECTION_RIGHT;
+  let visitedCount = 0;  
+  let row_idx = 0;
+  let col_idx = 0;
+  const matrixCount = row * col;
+  const isVisited = createMatrix( row, col );
+  while( visitedCount < matrixCount ) {
+    switch( direction )
+    {
+      case DIRECTION_RIGHT:
+        for( ; col_idx < col ; col_idx++ ) {
+          if( isVisited[row_idx][col_idx] ) break;
+          result += matrix[row_idx][col_idx];
+          isVisited[row_idx][col_idx] = true;
+          visitedCount++;
+        }
+        col_idx--;
+        row_idx++;
+        break;
+      case DIRECTION_DOWN:
+        for( ; row_idx < row ; row_idx++ ) {
+          if( isVisited[row_idx][col_idx] ) break;
+          result += matrix[row_idx][col_idx];
+          isVisited[row_idx][col_idx] = true;
+          visitedCount++;
+        }
+        row_idx--;
+        col_idx--;
+        break;
+      case DIRECTION_LEFT:
+        for( ; col_idx >= 0 ; col_idx-- ) {
+          if( isVisited[row_idx][col_idx] ) break;
+          result += matrix[row_idx][col_idx];
+          isVisited[row_idx][col_idx] = true;
+          visitedCount++;
+        }
+        col_idx++;
+        row_idx--;
+        break;
+      case DIRECTION_UP:
+        for( ; row_idx >= 0 ; row_idx-- ) {
+          if( isVisited[row_idx][col_idx] ) break;
+          result += matrix[row_idx][col_idx];
+          isVisited[row_idx][col_idx] = true;
+          visitedCount++;
+        }
+        row_idx++;
+        col_idx++;
+        break;
+    }
+    direction = ( direction + 1 ) % DIRECTION_OFFSET;
+  }
+  return result;
+};
+
 const rotateMatrix = function (matrix, k) {
   // TODO: 여기에 코드를 작성합니다.
   const rotateOnce = ( matrix ) => {
