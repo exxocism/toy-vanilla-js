7826e5339c7b16913d29b0fad516dc4f44954633
Charlie_eGPU <tchoi.tab@gmail.com>
2021-11-17 10:03:24 +0900

    robotpath2-fuckedup

--- vanilla-sample/function.js
+++ vanilla-sample/function.js
@@ -1,120 +1,184 @@
-const largestRectangularArea = function (histogram) {
-  // TODO: 여기에 코드를 작성합니다.
-  const getMaxArea = ( hist, n ) => {
-      // Create an empty stack. The stack holds indexes of hist[] array
-      // The bars stored in stack are always in increasing order of their heights.
-      const s = [];
-      Array.prototype.peek = function(){ return this[this.length-1]; }
-  
-      let max_area = 0; // Initialize max area
-      let tp; // To store top of stack
-      let area_with_top; // To store area with top bar as the smallest bar
-  
-      // Run through all bars of given histogram
-      let i = 0;
-      while (i < n) {
-          // If this bar is higher than the  bar on top stack, push it to stack
-          if (s.length === 0 || hist[s.peek()] <= hist[i]) s.push(i++);
-  
-          // If this bar is lower than top of stack, then calculate area of rectangle with
-          // stack top as the smallest (or minimum height) bar. 'i' is 'right index' for
-          // the top and element before top in stack is 'left index'
-          else {
-              tp = s.peek(); // store the top index
-              s.pop(); // pop the top
-  
-              // Calculate the area with hist[tp] stack as smallest bar
-              area_with_top = hist[tp] *
-                            (s.length === 0 ? i : i - s.peek() - 1);
-  
-              // update max area, if needed
-              if (max_area < area_with_top) max_area = area_with_top;
-          }
-      }
-  
-      // Now pop the remaining bars from stack and calculate area with every
-      // popped bar as the smallest bar
-      while (s.length > 0) {
-          tp = s.peek();
-          s.pop();
-          area_with_top = hist[tp] *
-                        (s.length == 0 ? i : i - s.peek() - 1);
-  
-          if (max_area < area_with_top) max_area = area_with_top;
-      }
-
-      return max_area; 
-  }
-
-  return getMaxArea( histogram, histogram.length );
-};
-
-// let histogram = [2, 1, 4, 5, 1, 3, 3];
-// let output = largestRectangularArea(histogram);
-// console.log(output); // --> 8
-let histogram = [6, 2, 5, 4, 5, 1, 6];
-let output = largestRectangularArea(histogram);
-console.log(output); // --> 12
-
-const rangeMinimum = function (arr, ranges) {
+const robotPath2 = function (room, src, sDir, dst, dDir) {
   // TODO: 여기에 코드를 작성합니다.
 
-  class segmentTree {
-    constructor( arr, start, end ) {
-      this.node = {
-        minimum: 0,
-        start: null,
-        end: null,
-        left: null,
-        right: null
-      };
-      if( arr ) this.fill( arr, start, end );
+  const MAX_ROW = room.length;
+  const MAX_COL = room[0].length;
+
+  // Fill in empty matrix
+  const adjacencyMatrix = Array(MAX_ROW).fill(0).map(() => Array(MAX_COL).fill(null));
+  const isVisited = Array(MAX_ROW).fill(0).map(() => Array(MAX_COL).fill(new Set()));
+
+  const checkAdjacency = ([row, col]) => {
+    
+    if( adjacencyMatrix[row][col] === null ) adjacencyMatrix[row][col] = new Set();
+
+    //check left column
+    for( let i = col - 1; i >= 0; i-- ) {
+      if( room[row][i] ) break;
+      if( row - 1 > 0 && room[row - 1][i] === 0 ) adjacencyMatrix[row][col].add(`${row},${i},0`);
+      if( row + 1 < MAX_ROW && room[row + 1][i] === 0 ) adjacencyMatrix[row][col].add(`${row},${i},180`);
     }
 
-    fill( arr, idx_start, idx_end ) {
-      //console.log( arr );
-      if( !arr?.length ) return ;
-      if( arr.length === 1 ) {
-        this.node.start = idx_start;
-        this.node.end = idx_end;
-        this.node.minimum = arr[0];
-        return ;
+    //check right column
+    for( let i = col + 1; i < MAX_COL; i++ ) {
+      if( room[row][i] ) break;
+      if( row - 1 > 0 && room[row - 1][i] === 0 ) adjacencyMatrix[row][col].add(`${row},${i},0`);
+      if( row + 1 < MAX_ROW && room[row + 1][i] === 0 ) adjacencyMatrix[row][col].add(`${row},${i},180`);
+    }
+
+    //check top row
+    for( let i = row - 1; i >= 0; i-- ) {
+      if( room[i][col] ) break;
+      if( col + 1 < MAX_COL && room[i][col + 1] === 0 ) adjacencyMatrix[row][col].add(`${i},${col},90`);
+      if( col - 1 > 0 && room[i][col - 1] === 0 ) adjacencyMatrix[row][col].add(`${i},${col},270`);
+    }
+
+    //check bottom row
+    for( let i = row + 1; i < MAX_ROW; i++ ) {
+      if( room[i][col] ) break;
+      if( col + 1 < MAX_COL && room[i][col + 1] === 0 ) adjacencyMatrix[row][col].add(`${i},${col},90`);
+      if( col - 1 > 0 && room[i][col - 1] === 0 ) adjacencyMatrix[row][col].add(`${i},${col},270`);
+    }
+
+  };
+
+  const ifDestinationGoable = (row, col) => {
+    if( row !== dst[0] && col !== dst[1] ) return false;
+    if( row === dst[0]) {
+
+      if( col < dst[1] ) {
+        for( let i = col + 1; i < dst[1]; i++ ) {
+          if( room[row][i] ) return false;
+        }
+      } else {
+        for( let i = col - 1; i > dst[1]; i-- ) {
+          if( room[row][i] ) return false;
+        }
       }
-      const mid = Math.ceil(arr.length / 2);
-      const idx_mid = Math.floor( (idx_end - idx_start) / 2) + idx_start;
-      this.node.start = idx_start;
-      this.node.end = idx_end;
-      this.node.minimum = Math.min( ...arr ); 
-      this.node.left = new segmentTree( arr.slice(0, mid), idx_start, idx_mid );
-      this.node.right = new segmentTree( arr.slice(mid), idx_mid + 1, idx_end );
-    }
+      return true;
 
-    minimum( left, right ) {
-      // 범위 밖에 있는 경우
-      if(left > this.node.end || right < this.node.start) return NaN;
-      // 범위 안에 있는 경우
-      if(left <= this.node.start && this.node.end <= right) return this.node.minimum;
-      // 그렇지 않다면 두 부분으로 나누어 합을 구하기
-      const minimum_left = this.node.left?.minimum( left, right );
-      const minimum_right = this.node.right?.minimum( left, right );
-      if( isNaN( minimum_left ) ) return minimum_right;
-      if( isNaN( minimum_right ) ) return minimum_left;
-      return Math.min( minimum_left, minimum_right );
+    } else {
+
+      if( row < dst[0] ) {
+        for( let i = row + 1; i < dst[0]; i++ ) {
+          if( room[i][col] ) return false;
+        }
+      } else {
+        for( let i = row - 1; i > dst[0]; i-- ) {
+          if( room[i][col] ) return false;
+        }
+      }
+      return true;
+
+    }
+  };
+
+  const calculateHeadings = (row, col, dstrow, dstcol) => {
+    if( row === dstrow ) {
+      if( col < dstcol ) return 90;
+      else return 270;
+    } else {
+      if( row < dstrow ) return 0;
+      else return 180;
     }
   }
 
-  const segtree = new segmentTree( arr, 0, arr.length - 1 );
-  const result = [];
-  ranges.forEach( ([start, end]) => {
-    result.push( segtree.minimum( start, end ) );
-  });
+  const getNextPosition = (row, col) => {
+    let positionDecided = false;
+    let nextPosition;
 
-  return result;
+    adjacencyMatrix[row][col].forEach( (position) => {
+      if( positionDecided || isVisited[row][col].has(position) ) return;
+      const [r, c, d] = position.split(',');
+      nextPosition.row = r;
+      nextPosition.col = c;
+      nextPosition.direction = d;
+      isVisited[row][col].add(position);
+      positionDecided = true;
+    });
+    return positionDecided? nextPosition : null;
+  }
+
+  const fallBackToLastAvailabePosition = (row, col) => {
+    //...ing
+  };
+
+  const caculateRollingTime = (srcDir, dstDir) => {
+    let rollinTime = 0;
+    while ( srcDir !== dstDir ) {
+      srcDir = (srcDir + 90) % 360;
+      rollinTime++;
+    }
+    return rollinTime;
+  }
+
+  for( let i = 0; i < MAX_ROW; i++ ) {
+    for( let j = 0; j < MAX_COL; j++ ) {
+      if( room[i][j] === 0 ) checkAdjacency([i, j]);
+    }
+  }
+
+  console.log(adjacencyMatrix);
+
+  const currentPostiion = {
+    row: src[0],
+    col: src[1],
+    direction: sDir
+  };
+  let elapsedTime = 0;
+
+  while ( currentPostiion.row !== dst[0] || currentPostiion.col !== dst[1] || currentPostiion.direction !== dDir ) {
+    
+    //finished
+    if( ifDestinationGoable(currentPostiion.row, currentPostiion.col) ) {
+
+      const targetDirection = calculateHeadings(currentPostiion.row, currentPostiion.col, dst[0], dst[1]);
+      if( currentPostiion.direction !== targetDirection ) {
+        //turn
+        elapsedTime += caculateRollingTime(currentPostiion.direction, targetDirection);
+        currentPostiion.direction = targetDirection;
+      }
+      //move
+      elapsedTime ++;
+      if( currentPostiion.direction !== dDir ) {
+        //turn
+        elapsedTime += caculateRollingTime(currentPostiion.direction, dDir);
+        currentPostiion.direction = dDir;
+      }
+      break;
+
+    }
+
+    const nextPosition = getNextPosition(currentPostiion.row, currentPostiion.col);
+    if( nextPosition === null ) {
+      //fall back to last available position
+
+    }
+
+    const targetDirection = calculateHeadings(currentPostiion.row, currentPostiion.col, nextPosition.row, nextPosition.col);
+    if( currentPostiion.direction !== targetDirection ) {
+      //turn
+      elapsedTime += caculateRollingTime(currentPostiion.direction, targetDirection);
+      currentPostiion.direction = targetDirection;
+    }
+    //move
+    currentPostiion.row = nextPosition.row;
+    currentPostiion.col = nextPosition.col;
+    elapsedTime++;
+  }
+
+  return elapsedTime;
 };
 
-/* const arr = [1, 3, 2, 7, 9, 11];
-const mins = rangeMinimum(arr, [
-  [1, 4],
-  [0, 3],
-]);
-console.log(mins); // --> [2, 1] */
+let room = [
+  [0, 0, 0, 0],
+  [0, 1, 1, 0],
+  [0, 1, 0, 0],
+  [0, 0, 1, 1],
+];
+let src = [3, 0];
+let sDir = 3;
+let dst = [2, 2];
+let dDir = 2;
+let output = robotPath2(room, src, sDir, dst, dDir);
+console.log(output); // --> 11
