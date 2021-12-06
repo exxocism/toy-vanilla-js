560109b97a52d55c6467c62af1c834eb63f6fd34
Charlie_eGPU <tchoi.tab@gmail.com>
2021-12-06 10:49:58 +0900

    countIslands

--- vanilla-sample/function.js
+++ vanilla-sample/function.js
@@ -1,184 +1,43 @@
-const robotPath2 = function (room, src, sDir, dst, dDir) {
+const countIslands = function (grid) {
   // TODO: 여기에 코드를 작성합니다.
+  const MAX_ROW = grid.length;
+  const MAX_COL = grid[0].length;
 
-  const MAX_ROW = room.length;
-  const MAX_COL = room[0].length;
-
-  // Fill in empty matrix
-  const adjacencyMatrix = Array(MAX_ROW).fill(0).map(() => Array(MAX_COL).fill(null));
-  const isVisited = Array(MAX_ROW).fill(0).map(() => Array(MAX_COL).fill(new Set()));
-
-  const checkAdjacency = ([row, col]) => {
+  const checkMap = ( row, col ) => {
+    const coords = [
+      [row - 1, col], //left
+      [row + 1, col], //right
+      [row, col - 1],
+      [row, col + 1] //down
+    ];
     
-    if( adjacencyMatrix[row][col] === null ) adjacencyMatrix[row][col] = new Set();
-
-    //check left column
-    for( let i = col - 1; i >= 0; i-- ) {
-      if( room[row][i] ) break;
-      if( row - 1 > 0 && room[row - 1][i] === 0 ) adjacencyMatrix[row][col].add(`${row},${i},0`);
-      if( row + 1 < MAX_ROW && room[row + 1][i] === 0 ) adjacencyMatrix[row][col].add(`${row},${i},180`);
-    }
-
-    //check right column
-    for( let i = col + 1; i < MAX_COL; i++ ) {
-      if( room[row][i] ) break;
-      if( row - 1 > 0 && room[row - 1][i] === 0 ) adjacencyMatrix[row][col].add(`${row},${i},0`);
-      if( row + 1 < MAX_ROW && room[row + 1][i] === 0 ) adjacencyMatrix[row][col].add(`${row},${i},180`);
-    }
-
-    //check top row
-    for( let i = row - 1; i >= 0; i-- ) {
-      if( room[i][col] ) break;
-      if( col + 1 < MAX_COL && room[i][col + 1] === 0 ) adjacencyMatrix[row][col].add(`${i},${col},90`);
-      if( col - 1 > 0 && room[i][col - 1] === 0 ) adjacencyMatrix[row][col].add(`${i},${col},270`);
-    }
-
-    //check bottom row
-    for( let i = row + 1; i < MAX_ROW; i++ ) {
-      if( room[i][col] ) break;
-      if( col + 1 < MAX_COL && room[i][col + 1] === 0 ) adjacencyMatrix[row][col].add(`${i},${col},90`);
-      if( col - 1 > 0 && room[i][col - 1] === 0 ) adjacencyMatrix[row][col].add(`${i},${col},270`);
-    }
-
-  };
-
-  const ifDestinationGoable = (row, col) => {
-    if( row !== dst[0] && col !== dst[1] ) return false;
-    if( row === dst[0]) {
-
-      if( col < dst[1] ) {
-        for( let i = col + 1; i < dst[1]; i++ ) {
-          if( room[row][i] ) return false;
-        }
-      } else {
-        for( let i = col - 1; i > dst[1]; i-- ) {
-          if( room[row][i] ) return false;
-        }
+    coords.forEach( ([r, c]) => {
+      if( r < 0 || r >= MAX_ROW || c < 0 || c >= MAX_COL ) return ;
+      if( grid[r][c] === '1' ) {
+        grid[r][c] = '2';
+        checkMap( r, c );
       }
-      return true;
-
-    } else {
-
-      if( row < dst[0] ) {
-        for( let i = row + 1; i < dst[0]; i++ ) {
-          if( room[i][col] ) return false;
-        }
-      } else {
-        for( let i = row - 1; i > dst[0]; i-- ) {
-          if( room[i][col] ) return false;
-        }
-      }
-      return true;
-
-    }
-  };
-
-  const calculateHeadings = (row, col, dstrow, dstcol) => {
-    if( row === dstrow ) {
-      if( col < dstcol ) return 90;
-      else return 270;
-    } else {
-      if( row < dstrow ) return 0;
-      else return 180;
-    }
-  }
-
-  const getNextPosition = (row, col) => {
-    let positionDecided = false;
-    let nextPosition;
-
-    adjacencyMatrix[row][col].forEach( (position) => {
-      if( positionDecided || isVisited[row][col].has(position) ) return;
-      const [r, c, d] = position.split(',');
-      nextPosition.row = r;
-      nextPosition.col = c;
-      nextPosition.direction = d;
-      isVisited[row][col].add(position);
-      positionDecided = true;
     });
-    return positionDecided? nextPosition : null;
-  }
-
-  const fallBackToLastAvailabePosition = (row, col) => {
-    //...ing
+    grid[row][col] = 2;
   };
 
-  const caculateRollingTime = (srcDir, dstDir) => {
-    let rollinTime = 0;
-    while ( srcDir !== dstDir ) {
-      srcDir = (srcDir + 90) % 360;
-      rollinTime++;
-    }
-    return rollinTime;
-  }
-
-  for( let i = 0; i < MAX_ROW; i++ ) {
-    for( let j = 0; j < MAX_COL; j++ ) {
-      if( room[i][j] === 0 ) checkAdjacency([i, j]);
-    }
-  }
-
-  console.log(adjacencyMatrix);
-
-  const currentPostiion = {
-    row: src[0],
-    col: src[1],
-    direction: sDir
-  };
-  let elapsedTime = 0;
-
-  while ( currentPostiion.row !== dst[0] || currentPostiion.col !== dst[1] || currentPostiion.direction !== dDir ) {
-    
-    //finished
-    if( ifDestinationGoable(currentPostiion.row, currentPostiion.col) ) {
-
-      const targetDirection = calculateHeadings(currentPostiion.row, currentPostiion.col, dst[0], dst[1]);
-      if( currentPostiion.direction !== targetDirection ) {
-        //turn
-        elapsedTime += caculateRollingTime(currentPostiion.direction, targetDirection);
-        currentPostiion.direction = targetDirection;
+  let count = 0;
+  for( let i = 0 ; i < MAX_ROW ; i ++ ) {
+    for( let j = 0 ; j < MAX_COL ; j ++ ) {
+      if( grid[i][j] === '1' ) {
+        count++;
+        checkMap( i, j );
       }
-      //move
-      elapsedTime ++;
-      if( currentPostiion.direction !== dDir ) {
-        //turn
-        elapsedTime += caculateRollingTime(currentPostiion.direction, dDir);
-        currentPostiion.direction = dDir;
-      }
-      break;
-
     }
-
-    const nextPosition = getNextPosition(currentPostiion.row, currentPostiion.col);
-    if( nextPosition === null ) {
-      //fall back to last available position
-
-    }
-
-    const targetDirection = calculateHeadings(currentPostiion.row, currentPostiion.col, nextPosition.row, nextPosition.col);
-    if( currentPostiion.direction !== targetDirection ) {
-      //turn
-      elapsedTime += caculateRollingTime(currentPostiion.direction, targetDirection);
-      currentPostiion.direction = targetDirection;
-    }
-    //move
-    currentPostiion.row = nextPosition.row;
-    currentPostiion.col = nextPosition.col;
-    elapsedTime++;
   }
 
-  return elapsedTime;
+  return count;
 };
 
-let room = [
-  [0, 0, 0, 0],
-  [0, 1, 1, 0],
-  [0, 1, 0, 0],
-  [0, 0, 1, 1],
+let grid = [
+  ['0', '1', '1', '1'],
+  ['0', '1', '1', '1'],
+  ['1', '1', '0', '0'],
 ];
-let src = [3, 0];
-let sDir = 3;
-let dst = [2, 2];
-let dDir = 2;
-let output = robotPath2(room, src, sDir, dst, dDir);
-console.log(output); // --> 11
+let result = countIslands(grid);
+console.log(result); // --> 1
--- vanilla-sample/text.js
+++ vanilla-sample/text.js
@@ -1,3 +1,39 @@
+const countIslands = function (grid) {
+  // TODO: 여기에 코드를 작성합니다.
+  const MAX_ROW = grid.length;
+  const MAX_COL = grid[0].length;
+
+  const checkMap = ( row, col ) => {
+    const coords = [
+      [row - 1, col], //left
+      [row + 1, col], //right
+      [row, col - 1],
+      [row, col + 1] //down
+    ];
+    
+    coords.forEach( ([r, c]) => {
+      if( r < 0 || r >= MAX_ROW || c < 0 || c >= MAX_COL ) return ;
+      if( grid[r][c] === '1' ) {
+        grid[r][c] = '2';
+        checkMap( r, c );
+      }
+    });
+    grid[row][col] = 2;
+  };
+
+  let count = 0;
+  for( let i = 0 ; i < MAX_ROW ; i ++ ) {
+    for( let j = 0 ; j < MAX_COL ; j ++ ) {
+      if( grid[i][j] === '1' ) {
+        count++;
+        checkMap( i, j );
+      }
+    }
+  }
+
+  return count;
+};
+
 function queuePrinter(bufferSize, capacities, documents) {
   // TODO: 여기에 코드를 작성합니다.
   // num_buffersize = 2, capacities = 10, [,,,], second = 8
