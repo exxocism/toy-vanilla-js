d8321c3be3f3685613b21684c17e01fa290e3a7a
Charlie_eGPU <tchoi.tab@gmail.com>
2021-12-07 01:05:41 +0900

    gossipprotocol

--- vanilla-sample/function.js
+++ vanilla-sample/function.js
@@ -1,43 +1,106 @@
-const countIslands = function (grid) {
-  // TODO: 여기에 코드를 작성합니다.
-  const MAX_ROW = grid.length;
-  const MAX_COL = grid[0].length;
+const createMatrix = (village) => {
+  const matrix = [];
+  village.forEach((line) => {
+    const row = [];
+    for (let i = 0; i < line.length; i++) row.push(line[i]);
+    matrix.push(row);
+  });
+  return matrix;
+};
 
-  const checkMap = ( row, col ) => {
-    const coords = [
-      [row - 1, col], //left
-      [row + 1, col], //right
-      [row, col - 1],
-      [row, col + 1] //down
-    ];
-    
-    coords.forEach( ([r, c]) => {
-      if( r < 0 || r >= MAX_ROW || c < 0 || c >= MAX_COL ) return ;
-      if( grid[r][c] === '1' ) {
-        grid[r][c] = '2';
-        checkMap( r, c );
-      }
+const gossipProtocol2 = function (village, num) {
+
+  const knowAll = ( ) => {
+    return village.every((line, y) => {
+      if( line.includes('1') ) return false;
+      return true;
     });
-    grid[row][col] = 2;
-  };
+  }
+  if( knowAll() ) return 0;
 
-  let count = 0;
-  for( let i = 0 ; i < MAX_ROW ; i ++ ) {
-    for( let j = 0 ; j < MAX_COL ; j ++ ) {
-      if( grid[i][j] === '1' ) {
-        count++;
-        checkMap( i, j );
+  const matrix = createMatrix(village);
+  const len = village.length;
+  const availableCoords = [];
+  let min = Infinity;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (matrix[i][j] === "2") {
+        matrix[i][j] = "X";
+        availableCoords.push([i, j, 0]);
       }
     }
   }
 
-  return count;
+  function dfs(L, s, queue) {
+    if(L === num) {
+      const result = bfs([...queue], createMatrix(village));
+      if(min > result) min = result;
+      return ;
+    }
+    for(let i = s; i < availableCoords.length; i++) {
+      queue.push(availableCoords[i]);
+      dfs(L + 1, i + 1, queue);
+      queue.pop();
+    }
+  }
+  dfs(0, 0, []);
+
+  return min;
 };
 
-let grid = [
-  ['0', '1', '1', '1'],
-  ['0', '1', '1', '1'],
-  ['1', '1', '0', '0'],
+function bfs(queue, matrix) {
+
+  const isNotEmpty = ( ) => {
+    for( let i = 0; i < matrix.length; i++) {
+      for( let j = 0; j < matrix[i].length; j++) {
+        if( matrix[i][j] === '1' ) return true;
+      }
+    }
+    return false;
+  }
+
+  const dy = [-1, 0, 1, 0];
+  const dx = [0, 1, 0, -1];
+  const len = matrix.length;
+  let result;
+  let index = 0;
+
+  while (index < queue.length) {
+    const [y, x, minutes] = queue[index++];
+    matrix[y][x] = "*";
+
+    result = minutes;
+    for (let k = 0; k < 4; k++) {
+      const ny = y + dy[k];
+      const nx = x + dx[k];
+      if (
+        nx >= 0 &&
+        ny >= 0 &&
+        nx < len &&
+        ny < len &&
+        ( matrix[ny][nx] === "1" || matrix[ny][nx] === "2" )
+      ) {
+        matrix[ny][nx] = "*";
+        queue.push([ny, nx, minutes + 1]);
+      }
+    }
+  }
+  
+  return isNotEmpty()? Infinity:result;
+}
+
+
+const village = [
+  '01102021',
+  '00101001',
+  '00111101',
+  '20000121',
+  '11000010',
+  '00000100',
+  '20200120',
+  '00000111',
 ];
-let result = countIslands(grid);
-console.log(result); // --> 1
+const num = 3;
+output = gossipProtocol2(village, num);
+console.log(output); // --> 3
